5fe0842f3e52556e92eaf86acf7dae553f37f545
ruslanpetrovup <ruslanpetrov163@gmail.com>
2023-04-07 19:03:23 +0300

    added menu

---
+++ js/solution-menu.js
@@ -0,0 +1,24 @@
+const menuSolution = document.querySelector(".solution-menu-list");
+
+const changeImage = ({ target }) => {
+  if (target.classList.value !== "solution-menu-link") return;
+
+  if (target.innerText.toLowerCase() === "qr codes") {
+    document.querySelector(".solution-image-img").src =
+      "./assets/images/solution/code.png";
+  } else if (target.innerText.toLowerCase() === "bio pages") {
+    document.querySelector(".solution-image-img").src =
+      "./assets/images/solution/bio.png";
+  } else if (target.innerText.toLowerCase() === "file hosting") {
+    document.querySelector(".solution-image-img").src =
+      "./assets/images/solution/file.png";
+  } else if (target.innerText.toLowerCase() === "devs api") {
+    document.querySelector(".solution-image-img").src =
+      "./assets/images/solution/api.png";
+  } else if (target.innerText.toLowerCase() === "help center") {
+    document.querySelector(".solution-image-img").src =
+      "./assets/images/solution/help.png";
+  }
+};
+
+menuSolution.addEventListener("mouseover", changeImage);
